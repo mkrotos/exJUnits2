3d1a6c9d35ca90e6c0a82585801afc14febd941d
Mariusz Krotos <mar.krotos@gmail.com>
2018-08-25 12:35:24 +0200

    clock

--- src/main/java/com/krotos/Main.java
+++ src/main/java/com/krotos/Main.java
@@ -4,12 +4,16 @@ package com.krotos;
 //stawienie oceny na zadaną liczbę gwiazdek 0-5
 //Pobranie aktualnej liczby gwiazdek
 
+
 public class Main {
     public static void main(String[] args) {
         ProductScore productScore = new ProductScore();
         System.out.println("Liczba gwiazdek przed ustawieniem oceny: " + productScore.getScore());
         productScore.setScore((short) 5);
         System.out.println("Liczba gwiazdek po ustawieniu: " + productScore.getScore());
+
+        Clock clock=new Clock();
+        System.out.println(clock.getTime());
     }
 
 
---
+++ src/test/java/com/krotos/ClockTest.java
@@ -0,0 +1,96 @@
+package com.krotos;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.DateTimeException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClockTest {
+    Clock clock;
+
+    @BeforeEach
+    public void before(){
+        clock=new Clock();
+    }
+    @Test
+    public void create(){
+        assertEquals(0,clock.getTime().getHour());
+        assertEquals(0,clock.getTime().getMinute());
+        assertEquals(0,clock.getTime().getSecond());
+    }
+    @Test
+    public void createWithProperParameters(){
+        clock=new Clock(15,10);
+        assertEquals(15,clock.getTime().getHour());
+        assertEquals(10,clock.getTime().getMinute());
+    }
+    @Test
+    public void createWithHoursLessThan0(){
+        assertThrows(DateTimeException.class,()->clock=new Clock(-2,0));
+    }
+    @Test
+    public void createWithHoursMoreThan23(){
+        assertThrows(DateTimeException.class,()->clock=new Clock(24,0));
+    }
+    @Test
+    public void createWithMinutesLessThan0(){
+        assertThrows(DateTimeException.class,()->clock=new Clock(11,-2));
+    }
+    @Test
+    public void createWithMinutesMoreThan59(){
+        assertThrows(DateTimeException.class,()->clock=new Clock(11,66));
+    }
+    @Test
+    public void addMinutes(){
+        clock=new Clock(10,10);
+        clock.addMinutes(20);
+        assertEquals(30,clock.getTime().getMinute());
+    }
+    @Test
+    public void addBigAmountOfMinutes(){
+        clock=new Clock(10,10);
+        clock.addMinutes(100);
+        assertEquals(50,clock.getTime().getMinute());
+        assertEquals(11,clock.getTime().getHour());
+    }
+    @Test
+    public void addBiggestAmountOfMinutes(){
+        clock=new Clock(10,10);
+        clock.addMinutes(30000);
+        assertEquals(10,clock.getTime().getMinute());
+        assertEquals(6,clock.getTime().getHour());
+    }
+    @Test
+    public void addNegativeMinutes(){
+        clock=new Clock(20,20);
+        clock.addMinutes(-10);
+        assertEquals(10,clock.getTime().getMinute());
+    }
+    @Test
+    public void subtractMinutes(){
+        clock=new Clock(20,20);
+        clock.subtractMinutes(10);
+        assertEquals(10,clock.getTime().getMinute());
+    }
+    @Test
+    public void addClockNull(){
+        Clock nullClock=null;
+        assertThrows(NullPointerException.class,()->clock.addClock(nullClock));
+    }
+    @Test
+    public void addProperClock(){
+        clock=new Clock(10,10);
+        Clock newClock=new Clock(10,11);
+        clock.addClock(newClock);
+        assertEquals(20,clock.getTime().getHour());
+        assertEquals(21,clock.getTime().getMinute());
+    }
+
+
+
+
+
+
+}
